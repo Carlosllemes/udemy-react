e96794a81d0d29aafd68a3b523c3aa8590fae749
Carlos Lemes <carlos.lemesxb@gmail.com>
2022-05-20 14:42:49 -0300

    aula-17-18

--- src/App.js
+++ src/App.js
@@ -2,38 +2,58 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: [
-        {
-          id: 1,
-          title: 'carlos 1',
-          body: 'corpo 1'
-        },
-        {
-          id: 2,
-          title: 'carlos 2',
-          body: 'corpo 2'
-        },
-        {
-          id: 3,
-          title: 'carlos 3',
-          body: 'corpo 3'
-        }
-      ]
-    }
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: 'carlos 1',
+        body: 'corpo 1'
+      },
+      {
+        id: 2,
+        title: 'carlos 2',
+        body: 'corpo 2'
+      },
+      {
+        id: 3,
+        title: 'carlos 3',
+        body: 'corpo 3'
+      }
+    ],
+    count : 0
+  }
+
+  timeOutUpdate = null
+
+  componentDidMount() {
+    this.handleTimeComponent()
+  }
+
+  componentDidUpdate(){
+    this.handleTimeComponent()
+  }
+
+  componentWillUnmount(){
+    clearTimeout(this.timeOutUpdate)
+  }
+
+  handleTimeComponent = () =>{
+    const { posts, count } = this.state
+    posts[0].title = 'Foifoi'
+    this.timeOutUpdate = setTimeout(() => {
+      this.setState({post:'foifoi', count : count +1})
+    }, 1000)
   }
 
   render() {
-    const { posts } = this.state
+    const { posts, count } = this.state
     return (
       <div className='App'>
         {posts.map(posts =>
         (
           <div key={posts.id}>
-            <h1>{posts.title}</h1>
-            <p>{posts.body}</p>
+            <h1>{count}</h1>
+            <h1>{posts.title}{posts.body}</h1>
           </div>
         )
         )}
