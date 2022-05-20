1fdb194f226a465d49490349c73ef3129dec06f3
Carlos Lemes <carlos.lemesxb@gmail.com>
2022-05-20 18:04:46 -0300

    aula-19

--- src/App.js
+++ src/App.js
@@ -3,60 +3,51 @@ import { Component } from 'react';
 
 class App extends Component {
   state = {
-    posts: [
-      {
-        id: 1,
-        title: 'carlos 1',
-        body: 'corpo 1'
-      },
-      {
-        id: 2,
-        title: 'carlos 2',
-        body: 'corpo 2'
-      },
-      {
-        id: 3,
-        title: 'carlos 3',
-        body: 'corpo 3'
-      }
-    ],
-    count : 0
+    posts: [],
   }
-
-  timeOutUpdate = null
 
   componentDidMount() {
-    this.handleTimeComponent()
+    this.loadPosts();
   }
 
-  componentDidUpdate(){
-    this.handleTimeComponent()
-  }
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
 
-  componentWillUnmount(){
-    clearTimeout(this.timeOutUpdate)
-  }
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
 
-  handleTimeComponent = () =>{
-    const { posts, count } = this.state
-    posts[0].title = 'Foifoi'
-    this.timeOutUpdate = setTimeout(() => {
-      this.setState({post:'foifoi', count : count +1})
-    }, 1000)
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((posts, index) => {
+      return { ...posts, cover: photosJson[index].url }
+    })
+    this.setState({ posts: postsAndPhotos });
   }
 
   render() {
     const { posts, count } = this.state
     return (
       <div className='App'>
-        {posts.map(posts =>
-        (
-          <div key={posts.id}>
-            <h1>{count}</h1>
-            <h1>{posts.title}{posts.body}</h1>
+        <div className="container">
+          <div className="row">
+
+            {posts.map(posts =>
+            (
+              <div key={posts.id} className="col-sm">
+                <div className="card">
+                  <img className="card-img-top" src={posts.cover} alt={posts.title} title={posts.title} />
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+            )
+            )}
           </div>
-        )
-        )}
+        </div>
       </div>
     )
   }
